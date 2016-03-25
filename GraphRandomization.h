d79fac57d2aea1087d027e63fe8c27f550812237
drzejek <drzejek@gmail.com>
2016-03-25 13:47:33 +0100

    Update GraphRandomization.h

--- GraphRandomization.h
+++ GraphRandomization.h
@@ -6,8 +6,11 @@ class GraphRandomization: public Graph
 public:
     GraphRandomization();
 
+/*Tworzy graf i generuje*/
     void MakeAndGenerate();
+/*Generuje graf jako macierz*/
     void GenerateAsMatrixNL();
+/*Randomizuje macierz*/
     void Randomize();
     void printMatrix();
     ~GraphRandomization();
