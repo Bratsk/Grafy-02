f8ea9ed21984d24950411be9bad34d9c4b86fdc0
drzejek <drzejek@gmail.com>
2016-03-25 14:09:51 +0100

    Update CheckSeqAndGenerate.h

--- CheckSeqAndGenerate.h
+++ CheckSeqAndGenerate.h
@@ -6,9 +6,13 @@ class CheckSeqAndGenerate: public Graph{
 
 public:
     CheckSeqAndGenerate(int *arraySeq, int NSize);
+    /*Liczy sume klasy tablicy*/
     int CountSum();
+    /*Liczy sume klasy tablicy danego jako argument*/
     int CountSumForArray(int *_SeqToCheck, int _arraySize);
+    /*Funkcja prawdzajaca czy dana sekwencja jest grafem*/
     bool CheckSeq();
+    /*Jeżeli dana sekwencja jest grafem, to gon generuje*/
     void GenerateGraph(bool isGraph);
     ~CheckSeqAndGenerate();
 protected:
